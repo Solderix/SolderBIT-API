fcb1f6f5fa212bf7b9fd5f8bdb2c3844361245e9
TomislavGrgicPulsar <tomislav.grgic@pulsarlabs.eu>
2025-11-28 13:37:33 +0100

    follower example

--- solderbit/examples/car_linefollower/main.py
+++ solderbit/examples/car_linefollower/main.py
@@ -5,7 +5,8 @@ import vehicle
 while not button_a.was_pressed():
     print("Press button A to start")
     sleep(1000)
-    
+
+vehicle.deadzone = 10    
 vehicle.move(x=0, y=0)
 
 trigger_value = 0.012
@@ -16,5 +17,4 @@ while True:
         direction = 1
     elif follower.read(3) < trigger_value:
         direction = -1
-    print(follower.read(0))
-    vehicle.move(x=direction*300, y=0)
+    vehicle.move(x=direction*400, y=80)
